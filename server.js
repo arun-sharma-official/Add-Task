a1f390a45b8654c5dd08fad4de623abfbe2e45b6
Arun Sir <arunsharma226931@gmail.com>
2026-09-23 07:12:20 +0530

    new changes

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ app.post('/delete/:id', async function(req,res){
 })
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is Runing.....")
 });
