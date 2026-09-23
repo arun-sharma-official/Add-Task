94e44a54b93e49bbffc3cd9681a637fff2b6ff23
Arun Sir <arunsharma226931@gmail.com>
2026-09-23 07:25:04 +0530

    ch

--- server.js
+++ server.js
@@ -11,9 +11,13 @@ app.set('view engine','ejs');
 
 connectdb();
 
-app.get('/', async function(req,res){
-    res.render('index');
-})
+// app.get('/', async function(req,res){
+//     res.render('index');
+// })
+
+app.get('/', function(req, res) {
+    res.send("Hello Render!");
+});
 
 app.post('/add',async function(req,res){
    await  course.create({
